9efff8cc227fe291bfcd6d23342ba6e5c7a8d8e8
lincolnyu <g@b1UtLhb9eN>
2017-04-22 02:13:00 +1000

    implemented a generic traversal.

--- Demeter/Management/TraversalHelper.cs
+++ Demeter/Management/TraversalHelper.cs
@@ -10,9 +10,23 @@ namespace Demeter.Management
         {
             public Recipe Recipe;
             public int Alt;
+            public double Serve;
         }
 
-        public static IEnumerable<IEnumerable<Dish>> Traverse(this IngredientDispenser dispenser, Stock stock, IList<Recipe> recipes, int start, double serve, IngredientDispenser.DispenseModes mode)
+        public delegate double GetServeCb(Recipe recipe, int alt, int level);
+
+        /// <summary>
+        ///  Returns all the possible sets of dishes that can be made out of the stock and the recipes
+        /// </summary>
+        /// <param name="dispenser">The unit that creates dishes by taking ingredients off from stock</param>
+        /// <param name="stock">The stock that provides the ingredients</param>
+        /// <param name="recipes">All recipes</param>
+        /// <param name="start">The recipe to start searching from for this traverse from the recipes</param>
+        /// <param name="getServe">The method that returns the quantity for the current dispensing</param>
+        /// <param name="mode">The mode in which the ingredients are dispensed</param>
+        /// <param name="allowDuplicates">Whether allow recipes to be used multiple times</param>
+        /// <returns>All the possible sets enumerated, each set is in the form of an enumeration of the dishes it contains</returns>
+        public static IEnumerable<IEnumerable<Dish>> Traverse(this IngredientDispenser dispenser, Stock stock, IList<Recipe> recipes, int start, GetServeCb getServe, IngredientDispenser.DispenseModes mode, bool allowDuplicates = false, int level = 0)
         {
             for (var i = start; i < recipes.Count; i++)
             {
@@ -20,19 +34,24 @@ namespace Demeter.Management
                 var altCount = dispenser.AltCount(recipe);
                 for (var alt = 0; alt < altCount; alt++)
                 {
+                    var serve = getServe(recipe, alt, level);
                     var dispensed = dispenser.Dispense(stock, recipe, serve, alt, mode);
                     if (dispensed.Any())
                     {
-                        var res = dispenser.Traverse(stock, recipes, i, serve, mode);
+                        var res = dispenser.Traverse(stock, recipes, allowDuplicates ? start : i
+                            , getServe, mode, allowDuplicates, level + 1);
                         foreach (var r in res)
                         {
-                            yield return Concat(new Dish { Recipe = recipe, Alt = alt }, r);
+                            yield return Concat(new Dish { Recipe = recipe, Alt = alt, Serve = serve }, r);
                         }
                         dispenser.Restore(stock, dispensed);
                     }
                 }
             }
         }
+
+        public static IEnumerable<IEnumerable<Dish>> Traverse(this IngredientDispenser dispenser, Stock stock, IList<Recipe> recipes, int start, double serve, IngredientDispenser.DispenseModes mode, bool allowDuplicates = false)
+            => dispenser.Traverse(stock, recipes, start, (r, a, l) => serve, mode, allowDuplicates);
 
         private static IEnumerable<Dish> Concat(Dish newHead, IEnumerable<Dish> dishes)
         {
